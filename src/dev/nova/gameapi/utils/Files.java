14b1e6f0eeffec05b2e53d9cd9f971059213929b
novastosha <galliigotu@gmail.com>
2021-09-11 08:32:19 +0100

    Added file properties.

--- src/dev/nova/gameapi/utils/Files.java
+++ src/dev/nova/gameapi/utils/Files.java
@@ -6,18 +6,30 @@ import sun.net.ftp.FtpClient;
 import java.io.File;
 
 public enum Files {
-    LOG_FOLDER(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"logs")),
-    PLAYERS(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"players")),
-    GAMES_FOLDER(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"games")),
-    GAME_FOLDER(new File(GAMES_FOLDER.getFile(),"NAME")),
-    GAME_MAPS_FOLDER(new File(GAME_FOLDER.getFile(),"maps")),
-    CONFIG(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"config.yml")),
-    PLUGINS(new File("./plugins"));
+    LOG_FOLDER(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"logs"),true,false),
+    PLAYERS(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"players"),true,false),
+    GAMES_FOLDER(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"games"),true,false),
+    GAME_FOLDER(new File(GAMES_FOLDER.getFile(),"NAME"),false,false),
+    GAME_MAPS_FOLDER(new File(GAME_FOLDER.getFile(),"maps"),false,false),
+    CONFIG(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"config.yml"),true,true),
+    PLUGINS(new File("./plugins"),false,false);
 
     private final File file;
+    private final boolean isFile;
+    private final boolean create;
 
-    Files(File file) {
+    Files(File file,boolean create, boolean isFile) {
         this.file = file;
+        this.create = create;
+        this.isFile = isFile;
+    }
+
+    public boolean isFile() {
+        return isFile;
+    }
+
+    public boolean isToCreate() {
+        return create;
     }
 
     public File getFile() {

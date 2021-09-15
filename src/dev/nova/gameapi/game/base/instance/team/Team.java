37450d501714332054852d4457daf6ee1490fc91
novastosha <galliigotu@gmail.com>
2021-09-15 09:16:55 +0100

    Added team size and addPlayer method to add players to a team.

--- src/dev/nova/gameapi/game/base/instance/team/Team.java
+++ src/dev/nova/gameapi/game/base/instance/team/Team.java
@@ -8,14 +8,27 @@ public class Team {
 
     private final String name;
     private final ArrayList<GamePlayer> players;
+    private final int size;
 
-    public Team(String name){
+    public Team(String name,int teamSize){
         this.name = name;
+        this.size =  teamSize;
         this.players = new ArrayList<GamePlayer>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void addPlayer(GamePlayer player){
+        players.add(player);
+        for(GamePlayer player1 : players){
+            player1.getPlayer().sendMessage("§6"+player.getPlayer().getName()+"§b joined your team! §6("+players.size()+"/"+size+")");
+        }
     }
 
     public ArrayList<GamePlayer> getPlayers() {

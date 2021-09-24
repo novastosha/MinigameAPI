3d58be0e6d5eaf9bc8984019ab6faa7a95e7d176
novastosha <galliigotu@gmail.com>
2021-09-24 09:21:04 +0100

    Game controllers (finally)

---
+++ src/dev/nova/gameapi/game/base/instance/controller/GameController.java
@@ -0,0 +1,14 @@
+package dev.nova.gameapi.game.base.instance.controller;
+
+public enum GameController {
+
+    WEATHER,
+    PLACE_BLOCKS,
+    BREAK_BLOCKS_MAP,
+    BREAK_BLOCKS,
+    ITEM_PICKUP,
+    ITEM_DROP,
+    ARROW_PICKUPS,
+    DEATH_DROPS;
+
+}

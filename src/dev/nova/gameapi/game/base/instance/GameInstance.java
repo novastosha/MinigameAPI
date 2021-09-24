422e0c602600f78633aef94b12bbdcdfc0701bca
novastosha <galliigotu@gmail.com>
2021-09-24 09:22:49 +0100

    GameID setting was rewritten, added controller and spectators support. (Spectators in a half working state)

--- src/dev/nova/gameapi/game/base/instance/GameInstance.java
+++ src/dev/nova/gameapi/game/base/instance/GameInstance.java
@@ -2,6 +2,7 @@ package dev.nova.gameapi.game.base.instance;
 
 import dev.nova.gameapi.game.base.GameBase;
 import dev.nova.gameapi.game.base.GameEvent;
+import dev.nova.gameapi.game.base.instance.controller.GameController;
 import dev.nova.gameapi.game.base.scoreboard.Scoreboard;
 import dev.nova.gameapi.game.base.values.GameValue;
 import dev.nova.gameapi.game.manager.GameManager;
@@ -9,14 +10,25 @@ import dev.nova.gameapi.game.map.GameMap;
 import dev.nova.gameapi.game.map.MapInjection;
 import dev.nova.gameapi.game.player.GamePlayer;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerPickupArrowEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.OverridingMethodsMustInvokeSuper;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public abstract class GameInstance {
 
@@ -29,12 +41,17 @@ public abstract class GameInstance {
     private final ArrayList<GamePlayer> players;
     private final ArrayList<GameEvent> events;
     private final ArrayList<GameValue<?>> values;
+    private final Map<GamePlayer,ArrayList<Block>> blocks;
 
     private final GameMap.Map map;
     private final GameUtils utils;
     private final String[] description;
     private final String brief;
     private final String displayName;
+    private final boolean spectatable;
+    private final Location spectatorLocation;
+    private final ArrayList<GamePlayer> spectators;
+    private final GameController[] controllers;
 
     private Scoreboard scoreboard;
 
@@ -48,12 +65,18 @@ public abstract class GameInstance {
      * 
      * @param map The map the players are going to play in.
      */
-    public GameInstance(String displayName,@Nonnull String gameBase, GameMap map,String[] gameDescription, String gameBrief) {
+    public GameInstance(String displayName,@Nonnull String gameBase, GameMap map,String[] gameDescription, String gameBrief,boolean spectatable, GameController[] gameControllers) {
         this.gameBase = GameManager.getGame(gameBase);
+        this.spectatable = spectatable;
+        this.spectators = new ArrayList<GamePlayer>();
         this.displayName = displayName;
         this.description =gameDescription;
         this.brief = gameBrief;
-        this.gameID = this.gameBase.getRunningInstances().size() +1;
+        this.blocks = new HashMap<>();
+        this.controllers = gameControllers;
+        this.gameID = this.gameBase.getRunningInstances().get(this.gameBase.getCode(getClass())) != null ?
+                this.gameBase.getRunningInstances().get(this.gameBase.getCode(getClass())).size() +1
+                : this.gameBase.getRunningInstances().size()+1;
         this.gameMap = map;
         this.events = new ArrayList<>();
         this.players = new ArrayList<GamePlayer>();
@@ -65,6 +88,8 @@ public abstract class GameInstance {
 
         this.utils = new GameUtils(this);
 
+        //this.spectatorLocation = spectatable ? this.map.loadOption("spectator-location", OptionType.LOCATION,true).getAsLocation() : null;
+        this.spectatorLocation = null;
     }
 
     protected void setScoreboard(Scoreboard scoreboard){
@@ -72,6 +97,30 @@ public abstract class GameInstance {
         for(GamePlayer player : players){
             getScoreboard().addPlayer(player);
         }
+    }
+
+    public GameController[] getControllers() {
+        return controllers;
+    }
+
+    public boolean containsController(GameController gameController){
+        for(GameController controller : controllers){
+            if(controller.equals(gameController)) return true;
+        }
+        return false;
+    }
+
+    public ArrayList<GamePlayer> getSpectators() {
+        return spectators;
+    }
+
+    public boolean isSpectatable() {
+        return spectatable;
+    }
+
+    @Nullable
+    public Location getSpectatorLocation() {
+        return spectatorLocation;
     }
 
     public String getDisplayName() {
@@ -155,11 +204,13 @@ public abstract class GameInstance {
                     injection.onEnd();
                 }
             }
+            this.gameBase.removeInstance(this);
         }
     }
 
     public void postEnd(){
         canStart = false;
+        this.gameBase.removeInstance(this);
     }
 
     @OverridingMethodsMustInvokeSuper
@@ -236,5 +287,92 @@ public abstract class GameInstance {
 
     }
 
-    public void onEvent(Event event){}
+    @OverridingMethodsMustInvokeSuper
+    public void onEvent(Event event){
+
+        if(event instanceof BlockPlaceEvent){
+            if(!containsController(GameController.PLACE_BLOCKS)) {
+                ((Cancellable) event).setCancelled(true);
+            }else{
+                GamePlayer player = GamePlayer.getPlayer(((BlockPlaceEvent) event).getPlayer());
+                ArrayList<Block> blocks = this.blocks.get(player) != null ? this.blocks.get(player) : new ArrayList<>();
+
+                blocks.add(((BlockPlaceEvent) event).getBlock());
+                this.blocks.put(player,blocks);
+            }
+        }
+        if(event instanceof BlockBreakEvent){
+            GamePlayer player = GamePlayer.getPlayer(((BlockBreakEvent) event).getPlayer());
+
+            boolean contains = false;
+
+            for( ArrayList<Block> blocksA : this.blocks.values()){
+                if(blocksA.contains(((BlockBreakEvent) event).getBlock())){
+                    contains = true;
+                    break;
+                }
+            }
+
+            if(!contains && !containsController(GameController.BREAK_BLOCKS_MAP)){
+                ((Cancellable) event).setCancelled(true);
+                player.getPlayer().sendMessage("§cYou cannot break map blocks!");
+            }
+
+            if(this.blocks.get(player).contains(((BlockBreakEvent) event).getBlock()) && !containsController(GameController.BREAK_BLOCKS)){
+                ((Cancellable) event).setCancelled(true);
+            }
+        }
+
+        if(event instanceof EntityPickupItemEvent && !containsController(GameController.ITEM_PICKUP)){
+            EntityPickupItemEvent pickupItemEvent = (EntityPickupItemEvent) event;
+            pickupItemEvent.setCancelled(true);
+        }
+
+        if(event instanceof PlayerDropItemEvent && !containsController(GameController.ITEM_DROP)){
+            ((Cancellable) event).setCancelled(true);
+        }
+
+        if(event instanceof PlayerPickupArrowEvent && ! containsController(GameController.ARROW_PICKUPS)){
+            ((Cancellable) event).setCancelled(true);
+        }
+
+        if(event instanceof PlayerDeathEvent){
+            PlayerDeathEvent deathEvent = (PlayerDeathEvent) event;
+
+            if(!containsController(GameController.DEATH_DROPS)) {
+                deathEvent.setDroppedExp(0);
+                deathEvent.setKeepInventory(true);
+                deathEvent.getEntity().getInventory().clear();
+
+            }
+        }
+
+        if(event instanceof WeatherChangeEvent && !containsController(GameController.WEATHER)){
+            ((Cancellable) event).setCancelled(true);
+            return;
+        }
+
+        GamePlayer player;
+        if(event instanceof PlayerChangedWorldEvent) {
+            player = GamePlayer.getPlayer(((PlayerChangedWorldEvent) event).getPlayer());
+        }else if(event instanceof PlayerQuitEvent){
+            player = GamePlayer.getPlayer(((PlayerQuitEvent) event).getPlayer());
+        }else{
+            return;
+        }
+
+        if(players.contains(player) || spectators.contains(player)){
+            players.remove(player);
+            spectators.remove(player);
+            player.getPlayer().sendMessage("§7You left the game with id: "+gameID);
+        }
+    }
+
+    public void switchToSpectator(GamePlayer player){
+        if(players.contains(player)){
+            players.remove(player);
+            spectators.add(player);
+            player.getPlayer().setGameMode(GameMode.SPECTATOR);
+        }
+    }
 }

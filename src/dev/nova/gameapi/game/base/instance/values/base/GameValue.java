3742dbd0ee8a1e42626808a1d238e9537ad5c330
novastosha <galliigotu@gmail.com>
2021-10-19 08:51:19 +0100

    Re-coded the GameValue stuff.

---
+++ src/dev/nova/gameapi/game/base/instance/values/base/GameValue.java
@@ -0,0 +1,50 @@
+package dev.nova.gameapi.game.base.instance.values.base;
+
+import dev.nova.gameapi.game.base.instance.GameInstance;
+
+import javax.annotation.OverridingMethodsMustInvokeSuper;
+
+public abstract class GameValue<T> {
+
+    private final T defaultValue;
+    private final String id;
+    private final GameInstance instance;
+    private final String displayName;
+    private T value;
+
+    public GameValue(GameInstance instance, String id, String displayName, T defaultValue){
+        this.id = id;
+        this.displayName = displayName;
+        this.instance = instance;
+        this.value = defaultValue;
+        this.defaultValue = defaultValue;
+
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public T getDefaultValue() {
+        return defaultValue;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+        instance.onValueUpdate(this);
+    }
+
+    @OverridingMethodsMustInvokeSuper
+    public boolean autoSetValue() {
+        instance.onValueUpdate(this);
+        return false;
+    }
+}

9a1c3e1d4c60b5d2b412d724cc672aaac68f90cb
novastosha <galliigotu@gmail.com>
2021-10-19 08:55:04 +0100

    Reformatted the code.

--- src/dev/nova/gameapi/game/base/tasks/GameEventListener.java
+++ src/dev/nova/gameapi/game/base/tasks/GameEventListener.java
@@ -2,6 +2,7 @@ package dev.nova.gameapi.game.base.tasks;
 
 import dev.nova.gameapi.game.base.GameBase;
 import dev.nova.gameapi.game.base.instance.GameInstance;
+import dev.nova.gameapi.game.base.instance.GameState;
 import dev.nova.gameapi.game.base.instance.eventListener.GlobalEventListener;
 import dev.nova.gameapi.game.base.scoreboard.player.PlayerScoreboardLine;
 import dev.nova.gameapi.game.manager.GameManager;
@@ -35,62 +36,64 @@ public class GameEventListener extends GlobalEventListener {
         for (GameBase base : GameManager.GAMES) {
             for (ArrayList<GameInstance> instances : base.getRunningInstances().values()) {
                 for (GameInstance instance : instances) {
-                    if (event instanceof PlayerDeathEvent) {
-                        PlayerDeathEvent deathEvent = (PlayerDeathEvent) event;
+                    if (!instance.getGameState().equals(GameState.ENDED) && !instance.getGameState().equals(GameState.CLOSED) && !instance.getGameState().equals(GameState.NOT_STARTED)) {
+                        if (event instanceof PlayerDeathEvent) {
+                            PlayerDeathEvent deathEvent = (PlayerDeathEvent) event;
 
-                        GamePlayer victim = GamePlayer.getPlayer(deathEvent.getEntity());
+                            GamePlayer victim = GamePlayer.getPlayer(deathEvent.getEntity());
 
-                        if (deathEvent.getEntity().getKiller() == null) {
-                            if (instance.getPlayers().contains(victim)) {
-                                instance.onEvent(event);
+                            if (deathEvent.getEntity().getKiller() == null) {
+                                if (instance.getPlayers().contains(victim)) {
+                                    instance.onEvent(event);
+                                }
+                            } else {
+                                GamePlayer killer = GamePlayer.getPlayer(deathEvent.getEntity().getKiller());
+                                if (instance.getPlayers().contains(victim) && instance.getPlayers().contains(killer)) {
+                                    instance.onEvent(event);
+
+                                }
                             }
-                        } else {
-                            GamePlayer killer = GamePlayer.getPlayer(deathEvent.getEntity().getKiller());
-                            if (instance.getPlayers().contains(victim) && instance.getPlayers().contains(killer)) {
-                                instance.onEvent(event);
-
-                            }
-                        }
-                        continue;
-                    }
-
-                    try {
-                        World world = (World) event.getClass().getMethod("getWorld").invoke(event);
-
-                        if (world != null) {
-                            if (instance.getMap() != null && world == instance.getMap().getBukkitWorld()) {
-                                instance.onEvent(event);
-                                continue;
-                            }
-                        }
-                        Player player = (Player) event.getClass().getMethod("getPlayer").invoke(event);
-
-                        if (player != null) {
-                            GamePlayer gamePlayer = GamePlayer.getPlayer(player);
-
-                            if (instance.getPlayers().contains(gamePlayer)) {
-                                instance.onEvent(event);
-                                continue;
-                            }
+                            continue;
                         }
 
-                        Entity entity = (Entity) event.getClass().getMethod("getEntity").invoke(event);
+                        try {
+                            World world = (World) event.getClass().getMethod("getWorld").invoke(event);
 
-                        if (entity != null) {
-                            if (!(entity instanceof Player)) {
-                                if (instance.getMap() != null && instance.getMap().getBukkitWorld().getEntities().contains(entity)) {
+                            if (world != null) {
+                                if (instance.getMap() != null && world == instance.getMap().getBukkitWorld()) {
                                     instance.onEvent(event);
                                     continue;
                                 }
-                            } else {
-                                GamePlayer player1 = GamePlayer.getPlayer((Player) entity);
-                                if (instance.getPlayers().contains(player1)) {
+                            }
+                            Player player = (Player) event.getClass().getMethod("getPlayer").invoke(event);
+
+                            if (player != null) {
+                                GamePlayer gamePlayer = GamePlayer.getPlayer(player);
+
+                                if (instance.getPlayers().contains(gamePlayer)) {
                                     instance.onEvent(event);
+                                    continue;
                                 }
                             }
+
+                            Entity entity = (Entity) event.getClass().getMethod("getEntity").invoke(event);
+
+                            if (entity != null) {
+                                if (!(entity instanceof Player)) {
+                                    if (instance.getMap() != null && instance.getMap().getBukkitWorld().getEntities().contains(entity)) {
+                                        instance.onEvent(event);
+                                        continue;
+                                    }
+                                } else {
+                                    GamePlayer player1 = GamePlayer.getPlayer((Player) entity);
+                                    if (instance.getPlayers().contains(player1)) {
+                                        instance.onEvent(event);
+                                    }
+                                }
+                            }
+                        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException ignored) {
+                            instance.onEvent(event);
                         }
-                    } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException ignored) {
-                        instance.onEvent(event);
                     }
                 }
             }

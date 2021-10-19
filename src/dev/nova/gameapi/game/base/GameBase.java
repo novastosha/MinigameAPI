5c302c0006945bb7c18b246b8b8e4313ad8c3199
novastosha <galliigotu@gmail.com>
2021-10-19 08:55:23 +0100

    Added lobby support of games.

--- src/dev/nova/gameapi/game/base/GameBase.java
+++ src/dev/nova/gameapi/game/base/GameBase.java
@@ -1,16 +1,29 @@
 package dev.nova.gameapi.game.base;
 
+import dev.nova.gameapi.GAPIPlugin;
 import dev.nova.gameapi.game.base.instance.GameInstance;
+import dev.nova.gameapi.game.base.lobby.Lobby;
+import dev.nova.gameapi.game.base.lobby.core.LobbyBase;
 import dev.nova.gameapi.game.map.GameMap;
 import dev.nova.gameapi.game.logger.GameLog;
 import dev.nova.gameapi.game.logger.GameLogger;
 import dev.nova.gameapi.game.logger.LogLevel;
 import dev.nova.gameapi.game.player.GamePlayer;
+import dev.nova.gameapi.utils.Files;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Difficulty;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 import javax.annotation.Nullable;
 import javax.annotation.OverridingMethodsMustInvokeSuper;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
@@ -26,15 +39,53 @@ public abstract class GameBase {
     private final ChatColor gameTheme;
     private final Map<String, Class<? extends GameInstance>> instances;
     private final Plugin plugin;
+    private final YamlConfiguration lobbyConfiguration;
+    private final ArrayList<Lobby> lobbyInstances;
+    private LobbyBase lobbyBase;
+    private boolean lobbySupport;
+    private final File lobbyFile;
 
-    public GameBase(Plugin plugin, String codeName, String displayName, Class<? extends GameInstance>[] gameInstances, ChatColor gameTheme) {
+    public GameBase(Plugin plugin, String codeName, String displayName, Class<? extends GameInstance>[] gameInstances, ChatColor gameTheme, boolean lobbySupport) {
         this.runningInstances = new HashMap<>();
+        this.lobbySupport = lobbySupport;
         this.codeName = codeName;
         this.plugin = plugin;
         this.displayName = displayName;
         this.gameMaps = new ArrayList<>();
+        this.lobbyInstances = new ArrayList<Lobby>();
         this.gameTheme = gameTheme;
         this.instances = new HashMap<>();
+        this.lobbyFile = new File(Files.getGameFolder(codeName), "lobby.yml");
+        this.lobbyConfiguration = new YamlConfiguration();
+
+        if (lobbySupport) {
+            if (!lobbyFile.exists()) {
+                try {
+                    lobbyFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    this.lobbySupport = false;
+                }
+            }
+            try {
+                lobbyConfiguration.load(lobbyFile);
+            } catch (IOException | InvalidConfigurationException e) {
+                e.printStackTrace();
+                this.lobbySupport = false;
+            }
+            World world = loadLobbyWorld();
+            String[] spawnData = loadSpawnData();
+
+            if (world != null && spawnData != null) {
+                this.lobbyBase = new LobbyBase(this, world, spawnData);
+            } else {
+                this.lobbySupport = false;
+            }
+        }
+
+        if (this.lobbySupport) {
+            createLobbyInstance();
+        }
 
         for (Class<? extends GameInstance> instance : gameInstances) {
             String code = getCode(instance);
@@ -45,6 +96,138 @@ public abstract class GameBase {
             }
             GameLogger.log(new GameLog(this, LogLevel.ERROR, "Unable to load game instance: " + instance.getSimpleName() + " as it does not contain a public static final String field named 'code'", true));
         }
+    }
+
+    private String[] loadSpawnData() {
+        ConfigurationSection spawnConfiguration = lobbyConfiguration.getConfigurationSection("spawn-data");
+
+        if (spawnConfiguration == null) {
+            this.lobbySupport = false;
+            Bukkit.getServer().getConsoleSender().sendMessage("§7[§bGameAPI§7] Spawn data configuration section is missing, for game: " + codeName + ", lobby support has been disabled!");
+            return null;
+        }
+
+        String[] spawnData = new String[5];
+
+        if (spawnConfiguration.contains("x") && spawnConfiguration.contains("y") && spawnConfiguration.contains("z")
+                && spawnConfiguration.contains("yaw") && spawnConfiguration.contains("pitch")) {
+
+
+            spawnData[0] = ((Double) spawnConfiguration.getDouble("x")).toString();
+            spawnData[1] = ((Double) spawnConfiguration.getDouble("y")).toString();
+            spawnData[2] = ((Double) spawnConfiguration.getDouble("z")).toString();
+
+            spawnData[3] = String.valueOf(Float.parseFloat(((Double) spawnConfiguration.getDouble("yaw")).toString()));
+            spawnData[4] = String.valueOf(Float.parseFloat(((Double) spawnConfiguration.getDouble("pitch")).toString()));
+        }
+        return spawnData;
+    }
+
+    public ArrayList<Lobby> getLobbyInstances() {
+        return lobbyInstances;
+    }
+
+    public Lobby createLobbyInstance() {
+        if (lobbySupport) {
+            Lobby lobby = new Lobby(lobbyBase, lobbyInstances.size() + 1);
+            lobbyInstances.add(lobby);
+            return lobby;
+        } else {
+            Bukkit.getServer().getConsoleSender().sendMessage("§7[§bGameAPI§7] Game: " + codeName + " invoked \"createLobbyInstance\" when lobby support was disabled!");
+            return null;
+        }
+    }
+
+    public void moveToLobby(GamePlayer player) {
+        if (lobbySupport) {
+            if (player.isInGame()) {
+                if (player.getGame().getPlayers().contains(player) || player.getGame().getSpectators().contains(player)) {
+                    player.getGame().leave(player, false);
+                }
+
+                Lobby lobby = getLobby(1);
+
+                if (lobby == null) {
+                    player.getPlayer().sendMessage("§cCould not find any lobby!");
+                    return;
+                }
+
+                lobby.move(player);
+            }
+        } else {
+            Bukkit.getServer().getConsoleSender().sendMessage("§7[§bGameAPI§7] Game: " + codeName + " invoked \"moveToLobby\" when lobby support was disabled!");
+            player.getPlayer().sendMessage("§cAttempted to move you to a lobby but lobby support for game: " + codeName + " is disabled!");
+        }
+    }
+
+    public Lobby getLobby(int placesNeeded) {
+        GAPIPlugin plugin = GAPIPlugin.getPlugin(GAPIPlugin.class);
+        int slots = plugin.getConfig().contains("max-lobby-players") ? plugin.getConfig().getInt("max-lobby-players") : 120;
+        if (lobbySupport) {
+            for (Lobby lobby : lobbyInstances) {
+
+                if (lobby.getWorld().getPlayers().size() + placesNeeded <= slots) {
+                    return lobby;
+                }
+            }
+            return null;
+        } else {
+            Bukkit.getServer().getConsoleSender().sendMessage("§7[§bGameAPI§7] Game: " + codeName + " invoked \"getLobby\" when lobby support was disabled!");
+            return null;
+        }
+    }
+
+    public File getLobbyFile() {
+        return lobbyFile;
+    }
+
+    public LobbyBase getLobbyBase() {
+        return lobbyBase;
+    }
+
+    public YamlConfiguration getLobbyConfiguration() {
+        return lobbyConfiguration;
+    }
+
+    private World loadLobbyWorld() {
+        String name = lobbyConfiguration.getString("world-folder");
+
+        if (name == null) {
+            lobbySupport = false;
+            Bukkit.getServer().getConsoleSender().sendMessage("§7[§bGameAPI§7] Bukkit world folder name is missing, for game: " + codeName + ", lobby support has been disabled!");
+            return null;
+        }
+
+        File worldFolder = new File(Bukkit.getWorldContainer(), name);
+
+        if (!worldFolder.exists()) {
+            lobbySupport = false;
+            Bukkit.getServer().getConsoleSender().sendMessage("§7[§bGameAPI§7] Bukkit world folder does not exist, for game: " + codeName + ", lobby support has been disabled!");
+            return null;
+        }
+
+        WorldCreator creator = new WorldCreator(name);
+        World newWorld = creator.createWorld();
+
+        newWorld.setAutoSave(true);
+        newWorld.setGameRuleValue("doDaylightCycle", "false");
+        newWorld.setGameRuleValue("doWeatherCycle", "false");
+        newWorld.setDifficulty(Difficulty.EASY);
+        newWorld.setTime(1000);
+        newWorld.setGameRuleValue("announceAdvancements", "false");
+        newWorld.setThundering(false);
+        newWorld.setStorm(false);
+        newWorld.setWeatherDuration(-1);
+
+        return newWorld;
+    }
+
+    public boolean hasLobbySupport() {
+        return lobbySupport;
+    }
+
+    public boolean onInitialize() {
+        return true;
     }
 
     public Plugin getPlugin() {
@@ -155,6 +338,33 @@ public abstract class GameBase {
     public void removeInstance(GameInstance gameInstance) {
         ArrayList<GameInstance> instances = getRunningInstances().get(getCode(gameInstance.getClass()));
         instances.remove(gameInstance);
+    }
+
+    public void moveToLobby(GamePlayer player, boolean b) {
+        if (lobbySupport) {
+
+            if (b && !player.isInGame()) {
+                return;
+            }
+
+            if (b) {
+                if (player.getGame().getPlayers().contains(player) || player.getGame().getSpectators().contains(player)) {
+                    player.getGame().leave(player, false);
+                }
+            }
+
+            Lobby lobby = getLobby(1);
+
+            if (lobby == null) {
+                player.getPlayer().sendMessage("§cCould not find any lobby!");
+                return;
+            }
+
+            lobby.move(player);
+        } else {
+            Bukkit.getServer().getConsoleSender().sendMessage("§7[§bGameAPI§7] Game: " + codeName + " invoked \"moveToLobby\" when lobby support was disabled!");
+            player.getPlayer().sendMessage("§cAttempted to move you to a lobby but lobby support for game: " + codeName + " is disabled!");
+        }
     }
 }
 

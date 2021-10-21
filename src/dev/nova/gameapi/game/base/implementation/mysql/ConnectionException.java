23cc8b96eddd93fe0be74c618fc6cda292ac7978
novastosha <galliigotu@gmail.com>
2021-10-21 20:09:11 +0100

    Rewrote the database connection.

--- src/dev/nova/gameapi/game/base/implementation/mysql/ConnectionException.java
+++
@@ -1,9 +0,0 @@
-package dev.nova.gameapi.game.base.implementation.mysql;
-
-public class ConnectionException extends Exception {
-
-    public ConnectionException(Exception e){
-        super(e);
-    }
-
-}
--- src/dev/nova/gameapi/game/base/implementation/mysql/authentication/DatabaseConnectionInfo.java
+++
@@ -1,6 +0,0 @@
-package dev.nova.gameapi.game.base.implementation.mysql.authentication;
-
-public record DatabaseConnectionInfo(String username, String password, int port,
-                                     String host) {
-
-}

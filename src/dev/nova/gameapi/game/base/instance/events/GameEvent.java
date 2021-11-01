fb460386e488577274fc3259cebb2fca1da32157
novastosha <galliigotu@gmail.com>
2021-11-01 21:50:04 +0100

    Rewrote the events system.

---
+++ src/dev/nova/gameapi/game/base/instance/events/GameEvent.java
@@ -0,0 +1,36 @@
+package dev.nova.gameapi.game.base.instance.events;
+
+import dev.nova.gameapi.game.base.instance.GameInstance;
+import dev.nova.gameapi.game.base.instance.events.exception.AlreadyInjectedException;
+
+import javax.annotation.OverridingMethodsMustInvokeSuper;
+
+public abstract class GameEvent {
+
+    public final int duration;
+    public int durationLeft;
+    public final String codeName;
+    public final String displayName;
+    protected GameInstance instance;
+
+    public GameEvent(String codeName, String displayName, int duration){
+        this.codeName = codeName;
+        this.displayName = displayName;
+        this.duration = duration;
+        this.durationLeft = duration;
+    }
+
+    @OverridingMethodsMustInvokeSuper
+    public void injectEvent(GameInstance instance) throws AlreadyInjectedException {
+        if(this.instance == null){
+            this.instance = instance;
+        }else{
+            throw new AlreadyInjectedException("Event: "+this.codeName+" is already injected to: "+this.instance.getGameID());
+        }
+    }
+
+    public abstract void onStart();
+    public abstract void onTick();
+    public abstract void onEnd();
+
+}

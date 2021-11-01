271d0078ae93d0b3baf8dd66d664a17e5255eabe
novastosha <galliigotu@gmail.com>
2021-11-01 21:42:52 +0100

    Added the exception that gets thrown when trying to inject an event that was already injected.

---
+++ src/dev/nova/gameapi/game/base/instance/events/exception/AlreadyInjectedException.java
@@ -0,0 +1,7 @@
+package dev.nova.gameapi.game.base.instance.events.exception;
+
+public class AlreadyInjectedException extends Exception {
+    public AlreadyInjectedException(String s) {
+        super(s);
+    }
+}

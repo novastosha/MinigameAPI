68cb574568dc13777947977b0ed174d0d5d67b1e
novastosha <galliigotu@gmail.com>
2021-11-01 21:56:42 +0100

    Added the party chat message listener

---
+++ src/dev/nova/gameapi/party/listener/ChatListener.java
@@ -0,0 +1,32 @@
+package dev.nova.gameapi.party.listener;
+
+import dev.nova.gameapi.game.player.GamePlayer;
+import dev.nova.gameapi.party.Party;
+import dev.nova.gameapi.party.settings.PartySettings;
+import net.kyori.adventure.text.Component;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+
+public class ChatListener implements Listener {
+
+    @EventHandler
+    public void onChat(PlayerChatEvent event){
+        GamePlayer player = GamePlayer.getPlayer(event.getPlayer());
+
+        if(player.isInParty() && player.partyChat){
+
+            if(PartySettings.MUTED.isEnabled(player.getParty()) && player != player.getParty().getLeader()){
+                Party.sendMessage(player, Component.text("§cThe party is muted"));
+                event.setCancelled(true);
+                return;
+            }
+
+            for(GamePlayer player1 : player.getParty().getPartyMembers().keySet()){
+                player1.getPlayer().sendMessage("§3"+player.getPlayer().getName()+"§7: "+event.getMessage());
+            }
+            event.setCancelled(true);
+        }
+    }
+
+}

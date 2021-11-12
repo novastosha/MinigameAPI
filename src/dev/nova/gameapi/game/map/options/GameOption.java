2aea28dd98cb1f4aecf2faaaa41e6c972179d0de
novastosha <galliigotu@gmail.com>
2021-11-12 19:41:30 +0100

    Added option for arrays of any type. and a value of any type (unsafe)

--- src/dev/nova/gameapi/game/map/options/GameOption.java
+++ src/dev/nova/gameapi/game/map/options/GameOption.java
@@ -2,6 +2,11 @@ package dev.nova.gameapi.game.map.options;
 
 import dev.nova.gameapi.game.map.GameMap;
 import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+
+import java.io.Serializable;
+import java.lang.reflect.Array;
 
 public class GameOption {
 
@@ -16,6 +21,8 @@ public class GameOption {
         this.type = optionType;
         this.value = value;
         this.map = map;
+
+        getAsArray(String.class);
     }
 
     public GameMap.Map getMap() {
@@ -34,15 +41,17 @@ public class GameOption {
         return value;
     }
 
-    public boolean getAsBoolean(){
+    public boolean getAsBoolean() {
         return (boolean) value;
     }
 
-    public String getAsString(){
+    public String getAsString() {
         return (String) value;
     }
 
-    public Location getAsLocation(){
+    public Location getAsLocation() { return getAsLocation(value); };
+
+    private Location getAsLocation(Object value) {
         Location location = (Location) value;
 
         return new Location(
@@ -54,5 +63,61 @@ public class GameOption {
                 location.getPitch()
         );
     }
+
+    public int getAsInt() {
+        return (int) getValue();
+    }
+
+    @SuppressWarnings("unchecked")
+    /**
+     *
+     * This method is not safe to use.
+     *
+     */
+    public <T> T get() {
+        return (T) getValue();
+    }
+
+    @SuppressWarnings("unchecked")
+    /**
+     *
+     *
+     * This method was marked as unchecked, but it is safe to use.
+     *
+     */
+    public <T> T[] getAsArray(Class<T> type) {
+
+        ConfigurationSection valueConfig = (ConfigurationSection) getValue();
+
+        T[] array = (T[]) Array.newInstance(type, valueConfig.getKeys(false).size());
+
+        int iter = 0;
+        for (String key : valueConfig.getKeys(false)) {
+            try {
+
+
+                T obj = null;
+                try {
+                    obj = (T) valueConfig.get(key);
+                } catch (ClassCastException e) {
+                    e.printStackTrace();
+                    continue;
+                }
+
+                if(obj instanceof Location) {
+                    obj = (T) getAsLocation(valueConfig.get(key));
+                }
+
+                array[iter] = obj;
+                iter++;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+        return array;
+    }
+
 
 }

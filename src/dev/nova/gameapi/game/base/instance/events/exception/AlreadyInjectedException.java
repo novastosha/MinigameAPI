009ec69be64e0e63deb517f539ab775736a575d1
novastosha <galliigotu@gmail.com>
2021-11-12 14:12:55 +0100

    Documented it.

--- src/dev/nova/gameapi/game/base/instance/events/exception/AlreadyInjectedException.java
+++ src/dev/nova/gameapi/game/base/instance/events/exception/AlreadyInjectedException.java
@@ -1,5 +1,10 @@
 package dev.nova.gameapi.game.base.instance.events.exception;
 
+/**
+ *
+ * An exception that gets thrown when an event class instance is already injected into another game!
+ *
+ */
 public class AlreadyInjectedException extends Exception {
     public AlreadyInjectedException(String s) {
         super(s);

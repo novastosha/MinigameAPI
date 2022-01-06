20d668c47572f806df3b7b8f72623d0d60192bc6
novastosha <galliigotu@gmail.com>
2022-01-06 20:20:39 +0100

    Added a method that gets executed when the file gets created.

--- src/dev/nova/gameapi/utils/Files.java
+++ src/dev/nova/gameapi/utils/Files.java
@@ -2,17 +2,76 @@ package dev.nova.gameapi.utils;
 
 import dev.nova.gameapi.GAPIPlugin;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public enum Files {
-    LOG_FOLDER(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"logs"),true,false),
-    PLAYERS(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"players"),true,false),
-    GAMES_FOLDER(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"games"),true,false),
-    GAME_FOLDER(new File(GAMES_FOLDER.getFile(),"NAME"),false,false),
-    GAME_MAPS_FOLDER(new File(GAME_FOLDER.getFile(),"maps"),false,false),
-    CONFIG(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"config.yml"),true,true),
-    PLUGINS(new File("./plugins"),false,false);
+    LOG_FOLDER(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"logs"),true,false){
+        @Override
+        public void onCreate() {
+
+        }
+    },
+    PLAYERS(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"players"),true,false){
+        @Override
+        public void onCreate() {
+
+        }
+    },
+    GAMES_FOLDER(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"games"),true,false){
+        @Override
+        public void onCreate() {
+
+        }
+    },
+    GAME_FOLDER(new File(GAMES_FOLDER.getFile(),"NAME"),false,false){
+        @Override
+        public void onCreate() {
+
+        }
+    },
+    GAME_MAPS_FOLDER(new File(new File(GAME_FOLDER.getFile(),"maps"),"MODE"),false,false){
+        @Override
+        public void onCreate() {
+
+        }
+    },
+    GAME_SCHEMATICS_FOLDER(new File(new File(GAME_FOLDER.getFile(),"schematics"),"MODE"),false,false){
+        @Override
+        public void onCreate() {
+
+        }
+    },
+    CONFIG(new File(GAPIPlugin.getPlugin(GAPIPlugin.class).getDataFolder(),"config.yml"),true,true){
+        @Override
+        public void onCreate() {
+            try {
+                FileWriter fileWriter = new FileWriter(getFile());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(GAPIPlugin.getPlugin(GAPIPlugin.class).getResource("config.yml")));
+
+                reader.lines().forEach(s -> {
+                    try {
+                        fileWriter.write(s+"\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                reader.close();
+                fileWriter.flush();
+                fileWriter.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    },
+    PLUGINS(new File("./plugins"),false,false){
+        @Override
+        public void onCreate() {
+
+        }
+    };
 
     private final File file;
     private final boolean isFile;
@@ -23,6 +82,8 @@ public enum Files {
         this.create = create;
         this.isFile = isFile;
     }
+
+    public abstract void onCreate();
 
     public boolean isFile() {
         return isFile;
@@ -52,7 +113,11 @@ public enum Files {
         return file;
     }
 
-    public static File getGameMapsFolder(String gameName){
-        return new File(GAME_MAPS_FOLDER.getFile().getPath().replaceAll("NAME",gameName));
+    public static File getGameMapsFolder(String gameName,String modeName){
+        return new File(GAME_MAPS_FOLDER.getFile().getPath().replaceAll("NAME",gameName).replaceAll("MODE",modeName));
+    }
+
+    public static File getGameSchematicsFolder(String gameName,String modeName){
+        return new File(GAME_SCHEMATICS_FOLDER.getFile().getPath().replaceAll("NAME",gameName).replaceAll("MODE",modeName));
     }
 }

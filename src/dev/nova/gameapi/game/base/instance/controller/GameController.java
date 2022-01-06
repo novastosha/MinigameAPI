c3608dff714d9fdb9b95a56e30a17a9e7c52c2ea
novastosha <galliigotu@gmail.com>
2022-01-06 20:25:28 +0100

    Added docs.

--- src/dev/nova/gameapi/game/base/instance/controller/GameController.java
+++ src/dev/nova/gameapi/game/base/instance/controller/GameController.java
@@ -1,10 +1,23 @@
 package dev.nova.gameapi.game.base.instance.controller;
 
+/**
+ *
+ * An enum class that has special options that affect the game and/or the world.
+ *
+ */
 public enum GameController {
 
+    /**
+     * Enables weather cycling
+     */
     WEATHER,
     PLACE_BLOCKS,
     BREAK_BLOCKS_MAP,
+    /**
+     *
+     * Enables the ability for players to break their blocks.
+     *
+     */
     BREAK_BLOCKS,
     ITEM_PICKUP,
     ITEM_DROP,

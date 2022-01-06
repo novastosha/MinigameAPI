7164b4e14531a5cb56f54b4e43e782886c2c0915
novastosha <galliigotu@gmail.com>
2022-01-06 20:39:47 +0100

    Added "some" support for coins database, disabling the plugin now deletes lobbies and added command registering

--- src/dev/nova/gameapi/GAPIPlugin.java
+++ src/dev/nova/gameapi/GAPIPlugin.java
@@ -6,43 +6,59 @@ package dev.nova.gameapi;
 import dev.nova.gameapi.game.base.GameBase;
 import dev.nova.gameapi.game.base.instance.GameInstance;
 import dev.nova.gameapi.game.base.instance.commands.LeaveCommand;
+import dev.nova.gameapi.game.base.instance.commands.ReviveCommand;
 import dev.nova.gameapi.game.base.instance.commands.SpectateCommand;
+import dev.nova.gameapi.game.base.instance.commands.SpectatorCommand;
+import dev.nova.gameapi.game.base.lobby.Lobby;
+import dev.nova.gameapi.game.base.mysql.DatabaseConnection;
 import dev.nova.gameapi.game.base.tasks.GameEventListener;
 import dev.nova.gameapi.game.base.tasks.GameTicker;
-import dev.nova.gameapi.game.base.tasks.ScoreboardTicker;
+import dev.nova.gameapi.game.base.tasks.LobbyScoreboardTicker;
 import dev.nova.gameapi.game.manager.GameManager;
-import dev.nova.gameapi.game.map.command.MapEditCommand;
-import dev.nova.gameapi.game.map.options.command.OptionCommands;
 import dev.nova.gameapi.game.queue.command.ForceStartCommand;
 import dev.nova.gameapi.game.queue.command.QueueCommand;
 import dev.nova.gameapi.party.command.PartyCommand;
-import dev.nova.gameapi.party.listener.ChatListener;
+import dev.nova.gameapi.game.base.listener.ChatListener;
 import dev.nova.gameapi.utils.Files;
+import dev.nova.gameapi.utils.api.schematics.commands.SchematicsSaveCommand;
+import dev.nova.gameapi.utils.api.schematics.commands.SchematicsWandCommand;
+import dev.nova.gameapi.utils.api.schematics.commands.listener.SchematicsInteractEventListener;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.ArrayList;
 
 public class GAPIPlugin extends JavaPlugin {
+
+    private static Connection coinsConnection;
+
+    public static Connection getCoinsConnection() {
+        return coinsConnection;
+    }
+
+    public static boolean isCoinsSystemHooked() {
+        return coinsConnection != null;
+    }
 
     @Override
     public void onEnable() {
 
         new GameEventListener(this, true);
-        Bukkit.getServer().getPluginManager().registerEvents(new ChatListener(),this);
+        Bukkit.getServer().getPluginManager().registerEvents(new ChatListener(), this);
+        Bukkit.getServer().getPluginManager().registerEvents(new SchematicsInteractEventListener(), this);
 
         QueueCommand queueCommand = new QueueCommand();
         getCommand("queue").setExecutor(queueCommand);
         getCommand("queue").setTabCompleter(queueCommand);
 
-        OptionCommands optionCommands = new OptionCommands();
-        getCommand("setOption").setExecutor(optionCommands);
-
-        getCommand("map").setExecutor(new MapEditCommand());
         getCommand("leave").setExecutor(new LeaveCommand());
+
+        getCommand("revive").setExecutor(new ReviveCommand());
+        getCommand("spectator").setExecutor(new SpectatorCommand());
 
         SpectateCommand spectateCommand = new SpectateCommand();
         getCommand("spectate").setExecutor(spectateCommand);
@@ -51,32 +67,45 @@ public class GAPIPlugin extends JavaPlugin {
         PartyCommand partyCommand = new PartyCommand();
         getCommand("party").setExecutor(partyCommand);
 
+        getCommand("wand").setExecutor(new SchematicsWandCommand());
+        getCommand("saveSchematic").setExecutor(new SchematicsSaveCommand());
+
         getCommand("forcestart").setExecutor(new ForceStartCommand());
 
         for (Files files : Files.values()) {
             if (files.isToCreate()) {
-                if (!files.isFile()) {
-                    if (!files.getFile().exists()) files.getFile().mkdirs();
-                } else {
-                    if (!files.getFile().exists()) {
+                if (!files.getFile().exists()) {
+                    if (!files.isFile()) {
+                        files.getFile().mkdirs();
+                    } else {
                         try {
                             files.getFile().createNewFile();
                         } catch (IOException ignored) {
 
                         }
+
                     }
+                    files.onCreate();
                 }
             }
         }
 
-        getConfig().options().copyDefaults(true);
+
+        if (getConfig().getBoolean("coins-support.enabled")) {
+            Bukkit.getServer().getConsoleSender().sendMessage("§7[§bGameAPI§7] Hooking coins system...");
+
+            String host = getConfig().getString("coins-support.host"), username = getConfig().getString("coins-support.username"),
+                    password = getConfig().getString("coins-support.password");
+
+            int port = getConfig().getInt("coins-support.port");
+
+            this.coinsConnection = new DatabaseConnection(host, port, username, password, "coins", "coins").connection;
+        }
 
         Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, GameTicker.instance(), 0L, 0L);
-        //Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, ScoreboardTicker.instance(), 0L, 0L);
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, LobbyScoreboardTicker.instance(), 0L, 0L);
 
         GameManager.loadGames();
-
-
     }
 
     private boolean deleteWorld(File path) {
@@ -97,6 +126,11 @@ public class GAPIPlugin extends JavaPlugin {
     public void onDisable() {
         Bukkit.getServer().getScheduler().cancelTasks(this);
         for (GameBase base : GameManager.GAMES) {
+            if (base.hasLobbySupport()) {
+                for (Lobby lobby : base.getLobbyInstances()) {
+                    lobby.delete();
+                }
+            }
             for (ArrayList<GameInstance> instances : base.getRunningInstances().values()) {
                 for (GameInstance instance : instances) {
                     Bukkit.getServer().getScheduler().cancelTask(instance.eventsTaskID);

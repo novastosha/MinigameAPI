546452f9768b134f4da909c3ac67dc94e41c7081
novastosha <galliigotu@gmail.com>
2022-01-06 20:15:29 +0100

    Class files

--- src/dev/nova/gameapi/game/base/scoreboard/ScoreboardLine.java
+++
@@ -1,9 +0,0 @@
-package dev.nova.gameapi.game.base.scoreboard;
-
-import dev.nova.gameapi.game.player.GamePlayer;
-
-public interface ScoreboardLine {
-
-    int getIndex();
-    String getAsString(Scoreboard scoreboard);
-}

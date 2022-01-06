e6c00970804f00631be7847bc90207fa68a84cfc
novastosha <galliigotu@gmail.com>
2022-01-06 20:25:59 +0100

    Added a built-in event for ending a game after certain time.

--- src/dev/nova/gameapi/game/base/instance/events/preset/GameEndEvent.java
+++ src/dev/nova/gameapi/game/base/instance/events/preset/GameEndEvent.java
@@ -10,33 +10,7 @@ public class GameEndEvent extends GameEvent {
     }
 
     @Override
-    public void onStart() {
-        String formatted = "";
-        if (duration > 60) {
-            formatted = String.format("%d min, %d sec",
-                    TimeUnit.MILLISECONDS.toMinutes(duration * 1000L),
-                    TimeUnit.MILLISECONDS.toSeconds(duration * 1000L) -
-                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration * 1000L))
-            );
-        } else if (duration == 60) {
-            formatted = "1 minute";
-        } else {
-            formatted = String.format("%d sec",
-                    TimeUnit.MILLISECONDS.toSeconds(duration * 1000L) -
-                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration * 1000L))
-            );
-        }
-        if (duration != 0) {
-            instance.getUtils().sendMessage("§c§lThe game is ending in: " + formatted);
-        }
-    }
-
-    @Override
-    public void onTick() {
-    }
-
-    @Override
-    public void onEnd() {
+    public void onEvent() {
         instance.onEnd();
     }
 }

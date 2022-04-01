c38dbbd674f8ab97b427983292543aec9d0350c8
novastosha <galliigotu@gmail.com>
2022-04-01 17:12:46 +0100

    Moved to 1.8

--- src/dev/nova/gameapi/game/base/instance/formats/tablist/TablistManager.java
+++ src/dev/nova/gameapi/game/base/instance/formats/tablist/TablistManager.java
@@ -2,10 +2,8 @@ package dev.nova.gameapi.game.base.instance.formats.tablist;
 
 import dev.nova.gameapi.game.base.instance.GameInstance;
 import dev.nova.gameapi.game.player.GamePlayer;
-import net.kyori.adventure.text.Component;
+import dev.nova.gameapi.utils.Titles;
 import org.bukkit.Bukkit;
-
-import java.lang.reflect.InvocationTargetException;
 
 public class TablistManager {
 
@@ -15,58 +13,16 @@ public class TablistManager {
 
         if (!instance.isTablistFormatNull()) {
             for (GamePlayer player : instance.getPlayers()) {
+                try {
+                    Titles.sendTabList(instance.getTablistHeaderFormat().apply(player),instance.getTablistLowerFormat().apply(player),player.getPlayer());
+                }catch (Exception ignored) {
 
-                setTablist(player,Component.text(instance.getTablistHeaderFormat().apply(player)),Component.text(instance.getTablistLowerFormat().apply(player)));
+                }
             }
-        }
-    }
-
-    private static void setTablist(GamePlayer player,Component header, Component footer) {
-        try {
-
-
-
-            Class<?> tablistPacketClass;
-            Object tablistPacket;
-            Class<?> rawPacketClass;
-            if (!serverVersion.startsWith("v1_17")) {
-                tablistPacketClass = Class.forName("net.minecraft.server." + serverVersion + ".PacketPlayOutPlayerListHeaderFooter");
-                rawPacketClass = Class.forName("net.minecraft.server." + serverVersion + ".Packet");
-                tablistPacket = tablistPacketClass.getDeclaredConstructor().newInstance();
-            } else {
-                Class<?> comp = Class.forName("net.minecraft.network.chat.IChatBaseComponent");
-                tablistPacketClass = Class.forName("net.minecraft.network.protocol.game.PacketPlayOutPlayerListHeaderFooter");
-                tablistPacket = tablistPacketClass.getConstructor(comp, comp).newInstance(null, null);
-                rawPacketClass = Class.forName("net.minecraft.network.protocol.Packet");
-            }
-
-
-            tablistPacket.getClass().getField("adventure$header").set(tablistPacket, header);
-            tablistPacket.getClass().getField("adventure$footer").set(tablistPacket, footer);
-
-            Class<?> rawCraftPlayer = Class.forName("org.bukkit.craftbukkit." + serverVersion + ".entity.CraftPlayer");
-
-            Object entityPlayer = rawCraftPlayer.getDeclaredMethod("getHandle").invoke(player.getPlayer());
-
-
-            Object connection;
-            if (!serverVersion.startsWith("v1_17")) {
-                connection = entityPlayer.getClass().getDeclaredField("playerConnection").get(entityPlayer);
-            } else {
-                connection = entityPlayer.getClass().getDeclaredField("b").get(entityPlayer);
-            }
-
-
-            connection.getClass().getMethod("sendPacket", rawPacketClass).invoke(connection, tablistPacket);
-
-
-        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException | NoSuchFieldException e) {
-            e.printStackTrace();
         }
     }
 
     public static void resetTablist(GamePlayer player) {
-        setTablist(player,Component.text(" "),Component.text(" "));
+        Titles.sendTabList("","", player.getPlayer());
     }
-
 }

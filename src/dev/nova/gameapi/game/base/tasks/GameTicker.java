295b2d302311c26dcb5356d2504681049ea1ccba
novastosha <galliigotu@gmail.com>
2022-04-01 17:29:25 +0100

    Added a condition that doesn't allow ticking when the game has ended

--- src/dev/nova/gameapi/game/base/tasks/GameTicker.java
+++ src/dev/nova/gameapi/game/base/tasks/GameTicker.java
@@ -2,6 +2,7 @@ package dev.nova.gameapi.game.base.tasks;
 
 import dev.nova.gameapi.game.base.GameBase;
 import dev.nova.gameapi.game.base.instance.GameInstance;
+import dev.nova.gameapi.game.base.instance.GameState;
 import dev.nova.gameapi.game.base.instance.formats.tablist.TablistManager;
 import dev.nova.gameapi.game.base.scoreboard.Scoreboard;
 import dev.nova.gameapi.game.manager.GameManager;
@@ -10,37 +11,37 @@ import dev.nova.gameapi.game.queue.Queue;
 import java.util.ArrayList;
 
 /**
- *
  * Simple class that handles game ticks.
- *
  */
 public class GameTicker implements Runnable {
     private static GameTicker instance;
 
     public static GameTicker instance() {
-        if(instance == null) instance = new GameTicker();
+        if (instance == null) instance = new GameTicker();
         return instance;
     }
 
-    private GameTicker(){
+    private GameTicker() {
 
     }
 
     @Override
     public void run() {
 
-            for(Queue queue : Queue.CLOSED_QUEUES) {
-                queue.coolDownTick();
-            }
+        for (Queue queue : Queue.CLOSED_QUEUES) {
+            queue.coolDownTick();
+        }
 
         for (GameBase base : GameManager.GAMES) {
-            synchronized (this) {
-                for(ArrayList<GameInstance> instances : base.getRunningInstances().values()) {
 
-                    for (GameInstance instance : instances) {
+            for (ArrayList<GameInstance> instances : base.getRunningInstances().values()) {
+
+                for (GameInstance instance : instances) {
+
+                    if (!instance.getGameState().equals(GameState.ENDED)) {
                         instance.tick();
 
-                        for(Scoreboard scoreboard : instance.getPlayerScoreboards()) {
+                        for (Scoreboard scoreboard : instance.getPlayerScoreboards()) {
                             scoreboard.updateScoreboard();
                         }
 
@@ -50,8 +51,8 @@ public class GameTicker implements Runnable {
 
                         instance.getGameMap().onTick(instance.getMap());
                     }
-
                 }
+
             }
         }
     }

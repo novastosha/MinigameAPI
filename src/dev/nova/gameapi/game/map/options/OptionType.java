19b5f8533bd13f1bf5d596525213fb12f8ec5a6a
novastosha <galliigotu@gmail.com>
2022-04-01 17:18:55 +0100

    Added support for map editing and other option types

--- src/dev/nova/gameapi/game/map/options/OptionType.java
+++ src/dev/nova/gameapi/game/map/options/OptionType.java
@@ -1,11 +1,24 @@
 package dev.nova.gameapi.game.map.options;
 
+import dev.nova.gameapi.game.map.options.instance.GameOptionInfo;
 public enum OptionType {
 
-    BOOLEAN,
-    STRING,
-    LOCATION,
-    INTEGER,
-    ARRAY,
-    CONFIGURATION_SECTION
+    BOOLEAN(null,"Boolean"),
+    STRING(null,"String"),
+    LOCATION(null,"Location"),
+    INTEGER(null,"Integer"),
+    ITEM(null,"Item"),
+    VECTOR(null,"Vector"),
+    DOUBLE(null,"Double"),
+    FLOAT(null,"Float"),
+    ARRAY(GameOptionInfo.OptionSpecificInfo.Array.class,"Array"),
+    CONFIGURATION_SECTION(GameOptionInfo.OptionSpecificInfo.ConfigurationSection.class,"Configuration Section");
+
+    public final Class<?> infoClass;
+    public final String displayName;
+
+    OptionType(Class<?> infoClass, String displayName) {
+        this.displayName = displayName;
+        this.infoClass = infoClass;
+    }
 }
